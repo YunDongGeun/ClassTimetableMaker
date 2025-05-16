16e32ed878d7eddbd1eb512aafb359f5d58f1a35
YunDongGeun <dong@geun.kr>
2025-05-16 19:43:02 +0900

    초기 커밋.

---
+++ ClassTimetableMaker/TimeTableBlock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTimetableMaker
+{
+    public class TimeTableBlock
+    {
+        public string ProfessorName { get; set; }
+        public string Classroom { get; set; }
+        public string Grade { get; set; }
+        public string CourseType { get; set; }
+        public bool IsFixedTime { get; set; }
+        public bool HasAdditionalRestrictions { get; set; }
+        public string UnavailableSlot1 { get; set; }
+        public string UnavailableSlot2 { get; set; }
+        public string AdditionalUnavailableSlot1 { get; set; }
+        public string AdditionalUnavailableSlot2 { get; set; }
+        public int CourseHour1 { get; set; }
+        public int CourseHour2 { get; set; }
+    }
+}
